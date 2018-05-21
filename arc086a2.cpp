01ce46367d1fc4ebee0d1730f93e437e6a58b9bd
tatsuya-yokoyama <ykym2011@gmail.com>
2018-05-22 08:29:11 +0900

    refactor

--- arc086a2.cpp
+++ arc086a2.cpp
@@ -4,12 +4,8 @@ using namespace std;
 
 int main() {
   int N;
-  int t[100001];
-  int x[100001];
-  int y[100001];
-  t[0] = 0;
-  x[0] = 0;
-  y[0] = 0;
+  int t[100001], x[100001], y[100001];
+  t[0] = x[0] = y[0] = 0;
   cin >> N;
   for (int i = 1; i <= N ; i++) {
     cin >> t[i] >> x[i] >> y[i];
@@ -20,26 +16,17 @@ int main() {
   for (int i = 0; i <= N - 1; i++) {
     int z = abs(x[i+1] - x[i]) + abs(y[i+1] - y[i]);
     int t_diff = t[i+1] - t[i];
-    //cout << "z:" << z << ",t_diff:" << t_diff << endl;
-    if (t_diff >= z) {
-      if (t_diff % 2 == 0 && z % 2 == 0) {
-        //cout << "even" << endl;
-        is_ok = true;
-      } else if (t_diff % 2 != 0 && z % 2 != 0) {
-        //cout << "odd" << endl;
-        is_ok = true;
-      } else {
-        cout << "No" << endl;
-        is_ok = false;
-        break;
-      }
-    } else {
-      cout << "No" << endl;
+    if (t_diff < z) {
       is_ok = false;
-      break;
+    }
+    // 偶数、奇数が一致する必要あり
+    if (z % 2 != t_diff % 2) {
+      is_ok = false;
     }
   }
   if (is_ok) {
     cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
   }
 }
