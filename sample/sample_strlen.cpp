aac087737677963fa435367f542a0085688b338b
tatsuya-yokoyama <ykym2011@gmail.com>
2018-08-02 21:19:16 +0900

    fix

---
+++ sample/sample_strlen.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+
+using namespace std;
+
+int main() {
+  string str = "ssgfrewiogsa";
+  string str2 = "google";
+  // string str3 = str + " " + str2;
+  // cout << str3 << endl;
+  cout << str.length() << endl;
+  if (str.empty()) {
+    cout << "emptuy!!" << endl;
+  } else {
+    cout << "not emptuy!!" << endl;
+  }
+  // for (int i = 0; i < 5; i++) {
+  //   string str3 = str
+  //   cout << str[i] + str[2] << endl;
+  // }
+}
