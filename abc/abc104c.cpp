e72f1ab4015871b36579e3e416d3daa26c8529e7
tatsuya-yokoyama <ykym2011@gmail.com>
2018-08-08 21:37:41 +0900

    fix

--- abc/abc104c.cpp
+++ abc/abc104c.cpp
@@ -5,21 +5,6 @@ using namespace std;
 int D, G;
 int p[11];
 int c[11];
-int dp[1000][100];
-
-int getMinNum(int i, int g) {
-  if (i == 0) {
-    return - 1;
-  }
-  int min_num1 = getMinNum(i+1, g);
-  int min_num2;
-  for (int j = 0; j < p[i]; j++) {
-    int sum = 100 * (1 + j);
-    min2 = min(min2, getMinNum(i+1, g-sum) + j + 1);
-  }
-  min2 = min(min2, getMinNum(i+1, g-sum-c[i]) + p[i]);
-  return min2;
-}
 
 int main() {
   // 入力
@@ -27,8 +12,40 @@ int main() {
   for (int i = 0; i < D; i++) {
     cin >> p[i] >> c[i];
   }
-  // 与えられたpiすべてを使っての、最小問題数を求める
-  int min_num = getMinNum(0, G);
-  cout << min_num << endl;
 
+  int min_num = 100 * 10;
+  // 全部解くor全く解かないで全探索
+  for (int bit = 0; bit < (1<<D); bit++) {
+    int S = 0;
+    int num = 0;
+    // cout << bitset<10>(bit) << endl;
+    for (int i = 0; i < D; i++) {
+      if (bit & (1<<i)) {
+        S += p[i] * 100 * (i+1) + c[i];
+        num += p[i];
+      }
+    }
+    if (S >= G) {
+      if (num < min_num) {
+        min_num = num;
+        // cout << bitset<10>(bit) << ", " << num << ", " << min_num << ", " << S << endl;
+      }
+    } else {
+      for (int i = D; i >= 0; i--) {
+        if ( !(bit & (1<<i)) ) {
+          for (int j = 0; j < p[i]; j++) {
+            S += 100 * (i+1);
+            num += 1;
+            if (S >= G) {
+              if (num < min_num) {
+                min_num = num;
+                // cout << bitset<10>(bit) << ", " << num << ", " << min_num << ", " << S << endl;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  cout << min_num << endl;
 }
