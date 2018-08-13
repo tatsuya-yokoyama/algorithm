4ae415d5883c9cf9686cdcd05cb279e8be60e5e0
tatsuya-yokoyama <ykym2011@gmail.com>
2018-08-13 22:03:20 +0900

    fix

--- aoj/alds1_7_C.cpp
+++ aoj/alds1_7_C.cpp
@@ -20,53 +20,66 @@ void preParse(int u) {
   preParse(T[u].r);
 }
 
+void inParse(int u) {
+  if (u == NIL) {
+    return;
+  }
+  inParse(T[u].l);
+  cout << " " << u;
+  inParse(T[u].r);
+}
+
 void postParse(int u) {
   if (u == NIL) {
     return;
   }
-  if (T[u].p !+ NIL) {
-    
-  }
-
+  postParse(T[u].l);
+  postParse(T[u].r);
+  cout << " " << u;
 }
 
 int main() {
   cin >> n;
 
+  // 初期化
   for (int i = 0; i < n; i++) {
     T[i].p = T[i].l = T[i].r = NIL;
   }
+  // 入力
   for (int i = 0; i < n; i++) {
     int v, l, r;
     cin >> v >> l >> r;
+    T[v].l = l;
+    T[v].r = r;
     if (l != NIL) {
-      T[v].l = l;
       T[l].p = v;
     }
     if (r != NIL) {
-      T[v].r = r;
       T[r].p = v;
     }
   }
 
-  int r;
+  // ルート求める
+  int root;
   for (int i = 0; i < n; i++) {
     int r;
     if (T[i].p == NIL) {
-      r = i;
+      root = i;
+      // cout << "root at " << root << endl;
+      break;
     }
   }
-
+  // cout << "root:" << root << endl;
   cout << "Preorder" << endl;
-  preParse(r);
+  preParse(root);
   cout << endl;
 
-  cout << "+Preorder" << endl;
-  preParse(r);
+  cout << "Inorder" << endl;
+  inParse(root);
   cout << endl;
-
-  cout << "-Preorder" << endl;
-  preParse(r);
+  //
+  cout << "Postorder" << endl;
+  postParse(root);
   cout << endl;
   return 0;
 
