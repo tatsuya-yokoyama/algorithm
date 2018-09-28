5d0035f1e2f3989a4b1ea8ddc9bbcd4bf15a8ddb
tatsuya-yokoyama <ykym2011@gmail.com>
2018-09-29 08:36:44 +0900

    fix

--- aizu/alds1_11_C.cpp
+++ aizu/alds1_11_C.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <queue>
+#define NIL -1
+#define N 110
 
 using namespace std;
 
+bool A[N][N];
+int d[N];
 int n;
-bool A[110][110];
-int d[110];
-int f[110];
-int t = 0;
-string color[110];
-queue<int> q;
 
-// void dfs(int u) {
-//   color[u] = "GRAY";
-//   d[u] = ++t;
-//   for (int v = 0; v < n; v++) {
-//     if (A[u][v] && color[v] == "WHITE") {
-//       // cout << "u:" << u+1 << " to " << v+1 << endl;
-//       dfs(v);
-//     }
-//   }
-//   color[u] = "BLACK";
-//   f[u] = ++t;
-// }
+void bfs(int s) {
+  queue<int> q;
+  // 距離初期化
+  for (int i = 0; i < N; i++) d[i] = NIL;
+  q.push(s);
+  d[s] = 0;
+  while(!q.empty()) {
+    int u = q.front(); q.pop();
+    for (int v = 0; v < n; v++) {
+      if (A[u][v] && d[v] == NIL) {
+        q.push(v);
+        d[v] = d[u] + 1;
+        // cout << "from:" << u+1 << ", to:" << v+1 << endl;
+      }
+    }
+  }
+}
 
 int main() {
-
 
   // 隣接行列を初期化
   cin >> n;
@@ -34,16 +36,12 @@ int main() {
       A[i][j] = false;
     }
   }
-  // 距離初期化
-  for (int i = 0; i < 110; i++) {
-    d[i] = -1;
-  }
+
   // 隣接行列に書き込み & ノードをwhiteに
   for (int i = 0; i < n; i++) {
     int u, k, v;
     cin >> u >> k;
     u--;
-    color[u] = "WHITE";
     for (int j = 0; j < k; j++) {
       cin >> v;
       v--;
@@ -51,24 +49,9 @@ int main() {
     }
   }
 
-  q.push(0);
-  d[0] = 0;
-  color[0] = "GRAY";
-  while(!q.empty()) {
-    int u = q.front();
-    q.pop();
-    for (int v = 0; v < n; v++) {
-      if (A[u][v] && color[v] == "WHITE") {
-        q.push(v);
-        d[v] = d[u] + 1;
-        color[v] = "GRAY";
-        // cout << "from:" << u+1 << ", to:" << v+1 << endl;
-      }
-    }
-    color[u] = "BLACK";
-  }
+  // bfs
+  bfs(0);
 
-  // cout << "---------" << endl;
   // print
   for (int i = 0; i < n; i++) {
     cout << i+1 << " " << d[i] << endl;
