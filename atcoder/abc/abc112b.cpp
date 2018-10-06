9b9a1104f3e1abe55c458e8593fabdd2961b6667
tatsuya-yokoyama <ykym2011@gmail.com>
2018-10-07 08:51:18 +0900

    fix

--- atcoder/abc/abc112b.cpp
+++ atcoder/abc/abc112b.cpp
@@ -5,22 +5,18 @@ using namespace std;
 int main() {
   int N, T;
   cin >> N >> T;
-  int min_cost = 100000;
-  int min_cost_id = -1;
+  int MAX = 1 << 20;
+  int min_cost = MAX;
   for (int i = 0; i < N; i++) {
     int c, t;
     cin >> c >> t;
     if (t <= T) {
-      // cout << c << "," << t << endl;
-      if (c < min_cost) {
-        min_cost = c;
-        min_cost_id = i + 1;
-      }
+      min_cost = min(c, min_cost);
     }
   }
-  if (min_cost_id == -1) {
-    cout << "TLE" << endl;
-  } else{
+  if (min_cost < MAX) {
     cout << min_cost << endl;
+  } else {
+    cout << "TLE" << endl;
   }
 }
