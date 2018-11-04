f11dfb8fca0e5e017ad2d773b8a5c3862f9c946d
tatsuya-yokoyama <ykym2011@gmail.com>
2018-11-04 16:24:23 +0900

    fix

--- aizu/alds1_11_A.cpp
+++ aizu/alds1_11_A.cpp
@@ -3,34 +3,30 @@
 using namespace std;
 
 int main() {
-  // 初期化
   int n;
   cin >> n;
-  int a[101][101];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      a[i][j] = 0;
+  int A[101][101];
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      A[i][j] = 0;
     }
   }
-  for (int i = 0; i < n; i++) {
-    int u, k;
+
+  int u, v;
+  int k;
+  for (int i = 0; i < n; ++i) {
     cin >> u >> k;
-    u--;
-    for (int j = 0; j < k; j++) {
-      int u_a;
-      cin >> u_a;
-      u_a--;
-      // cout << "u:" << u << ", u_a" << u_a << endl;
-      a[u][u_a] = 1;
+    for (int j = 0; j < k; ++j) {
+      cin >> v;
+      A[u-1][v-1] = 1;
     }
   }
-  // cout << endl;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (j) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (j > 0) {
         cout << " ";
       }
-      cout << a[i][j];
+      cout << A[i][j];
     }
     cout << endl;
   }
