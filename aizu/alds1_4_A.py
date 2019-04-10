c534e805c0af23310c79d4f042b56e87e49e66f8
tatsuya-yokoyama <ykym2011@gmail.com>
2019-04-10 19:14:59 +0900

    fix

--- aizu/alds1_4_A.py
+++ aizu/alds1_4_A.py
@@ -1,9 +1,26 @@
 
-def linear_search(target, nums):
+def linear_search(key, nums):
     for num in nums:
-        if num == target:
+        if num == key:
             return True
     return False
+
+def binary_search(key, nums):
+    # print('binary_serach ' + str(key))
+    left = 0
+    right = len(nums)
+    mid = int((left+right)/2)
+
+    while left < right:
+        mid = int((left+right)/2)
+        # print(left, mid, right)
+        if nums[mid] == key:
+            return key
+        elif key < nums[mid]:
+            right = mid
+        else:
+            left = mid + 1
+    return -1
 
 n = int(input())
 S = list(map(int, input().split()))
@@ -12,7 +29,7 @@ T = list(map(int, input().split()))
 
 count = 0
 for t in T:
-    if linear_search(t, S):
+    if binary_search(t, S) > -1:
         count += 1
         
 print(count)
