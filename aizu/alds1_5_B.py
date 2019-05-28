9bea91b7aa25266bc02166e53796dab1c1000bdd
tatsuya-yokoyama <ykym2011@gmail.com>
2019-05-28 23:18:24 +0900

    fix

---
+++ aizu/alds1_5_B.py
@@ -0,0 +1,31 @@
+def merge(A, left, mid, right):
+    global count
+    inf = 10 ** 9
+    L = A[left:mid] + [inf]
+    R = A[mid:right] + [inf]
+
+    i = 0
+    j = 0
+    for k in range(left, right):
+        count += 1
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+
+def mergeSort(A, left, right):
+    if left + 1 < right:
+        mid = (left + right) // 2
+        mergeSort(A, left, mid)
+        mergeSort(A, mid, right)
+        merge(A, left, mid, right)
+
+n = int(input())
+A = list(map(int, input().split()))
+count = 0
+mergeSort(A, 0, n)
+
+print(' '.join(list(map(str, A))))
+print(count)
